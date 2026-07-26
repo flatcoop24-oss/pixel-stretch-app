97ae3c12fbaff11d70f969bcd99334c6a7456f5d
flatcoop24-oss <flatcoop24@gmail.com>
2026-07-26 18:58:49 +0900

    Bump cache for true stretch and layer order release

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pixel-stretch-v13';
+const CACHE='pixel-stretch-v14';
 const ASSETS=['./','./index.html','./style.css','./app.js','./app-effects.js','./app-segmentation.js','./app-interactions.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
